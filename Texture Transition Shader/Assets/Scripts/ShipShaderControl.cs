80eb5ebbaab6245a507af3c2bfb879ad2932f449
ElliotChester <elliotchester97@gmail.com>
2018-05-25 00:12:08 +0100

    Updated Shader With UI

--- Texture Transition Shader/Assets/Scripts/ShipShaderControl.cs
+++ Texture Transition Shader/Assets/Scripts/ShipShaderControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipShaderControl : MonoBehaviour {
 
@@ -8,28 +9,46 @@ public class ShipShaderControl : MonoBehaviour {
     public float ShipDisolveTarget;
     public float ShipDisolveAmount;
 
+    public Image previewTransitionImage;
+    public GameObject previewRenderer;
+
+    public Sprite[] sprites;
+
     // Use this for initialization
     void Start () {
-		
+        SwitchTransition(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         ShipDisolveAmount = Mathf.MoveTowards(ShipDisolveAmount, ShipDisolveTarget, TransitionSpeed);
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if(ShipDisolveTarget == 1)
-            {
-                ShipDisolveTarget = 0;
-            }else
-            if (ShipDisolveTarget == 0)
-            {
-                ShipDisolveTarget = 1;
-            }
+            Transition();
         }
 
         this.GetComponent<Renderer>().material.SetFloat("DissolveAmount", ShipDisolveAmount);
+        previewRenderer.GetComponent<Image>().material.SetFloat("DissolveAmount", ShipDisolveAmount);
+    }
 
-	}
+    public void Transition()
+    {
+        if (ShipDisolveTarget == 1)
+        {
+            ShipDisolveTarget = 0;
+        }
+        else
+            if (ShipDisolveTarget == 0)
+        {
+            ShipDisolveTarget = 1;
+        }
+    }
+
+    public void SwitchTransition(int id)
+    {
+        this.GetComponent<Renderer>().material.SetTexture("TransitionTexture", sprites[id].texture);
+        previewRenderer.GetComponent<Image>().material.SetTexture("TransitionTexture", sprites[id].texture);
+        previewTransitionImage.sprite = sprites[id];
+    }
 }
